be216a4ed8c6f480c579a536b0539a7aa5427a91
Victor Guerra <v.e.g.a.2000@hotmail.com>
2020-09-15 22:41:39 -0600

    Update Item functionality

--- controller/itemController.js
+++ controller/itemController.js
@@ -4,13 +4,18 @@ const Item = require('../models/item');
 const im = require('./itemManager');
 
 router.get('/:restaurantId/item/:itemId', async (req,res) => {
-
+    const response = im.getItem(req.params.itemId);
+    res.json(await response);
 });
 
 router.put('/:restaurantId/item/:itemId', async (req,res) => {
-    
+   const response = im.updateItem(req.params.itemId, req.body);
+   res.send(await response);
 });
 
 router.get('/:restaurantId/item', async (req,res) => {
-
+    const response = await im.getItems();
+    res.json(await response);
 });
+
+module.exports = router;
--- controller/itemManager.js
+++ controller/itemManager.js
@@ -2,14 +2,42 @@ const express = require('express');
 const Item = require('../models/item'); 
 
 async function getItem(itemId) {
-
+    try {
+        const item = await Item.findById(itemId);
+        return item;
+    } catch (err) {
+        return err;
+    }
 }
 
-async function updateItem(itemId) {
+async function updateItem(itemId, itemBody) {
+    try {
+        // const updateOps = {};
+        // for(const op of itemBody){
+            //updateOPs[ops.propname] = ops.value;
+            //}
+        // en Update: { $set: updateOps } 
+        await Item.findByIdAndUpdate(itemId, { $set: { 
+                                             name: itemBody.name,
+                                             description: itemBody.description,
+                                             price: itemBody.price,
+                                             image: itemBody.image,
+                                             availability: itemBody.availability,
+                                             includedSides: itemBody.includedSides }});
+        return 'Item Updated'
+    } catch(err) {
+        return err;
+    }
 
 }
 
 async function getItems() {
+    try {
+        const items = await Item.find();
+        return items;
+    } catch(err){
+        return err;
+    }
 
 }
 
