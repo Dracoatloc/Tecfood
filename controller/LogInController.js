6eae27eac4f24851c22e8c7d789b9b98dd87c7de
AQJMTZ <aqjmtz@outlook.com>
2020-10-05 13:48:02 -0600

    TC2-LogIn added

--- controller/LogInController.js
+++ controller/LogInController.js
@@ -11,19 +11,19 @@ let customerDAO = require("../DAO/customerDAO");
 router.post("/login", (req, res, next) => {
     //const password = customerDAO.getCustomerPassword(req.body.password);
     //const email = customerDAO.getCustomerEmail(req.body.email)
-    Customer.findOne({email: req.body.email })
+    Customer.find({email: req.body.email })
     .exec()
     .then(customer => {
         if (customer.length < 1) {
             return res.status(401).json({
-                message: 'Authentication Failed'
+                message: 'Authentication Failed:Email doesn\'t exist'
             });
         }
        
         bcrypt.compare(req.body.password, customer[0].password, (err, result)=>{
             if (err){
                 return res.status(401).json({
-                    message: 'Authentication Failed'
+                    message: 'Authentication Failed: wrong password'
                 });
             }
             if(result){
@@ -33,7 +33,7 @@ router.post("/login", (req, res, next) => {
 
             }
             res.status(401).json({
-                message: 'Authentication failed'
+                message: 'Authentication failed: Wrong password'
             });
 
         });
